129b931abfe24137c4b88749c2dd1a6aaddb3cf4
Marco Santos <marcotunas@gmail.com>
2019-09-17 20:51:52 +0100

    Add validations torwards MQTT Store

--- glartek-collector.js
+++ glartek-collector.js
@@ -4,43 +4,53 @@ module.exports = function(RED) {
     const path = require('path');
     const fs = require('fs');
     const mqtt = require('mqtt'),
-        NeDBStore = require('mqtt-nedb-store');
+    NeDBStore = require('mqtt-nedb-store');
 
     function GlartekCollectorNode(config) {
         const node = this;
 
         RED.nodes.createNode(node, config);
 
-        node.warn('config.broker: ' + config.broker);
-        node.warn('config.id: ' + config.id);
-        node.warn('config.topic: ' + config.topic);
-
-        const dir = path.join(RED.settings.userDir, 'glartek-collector');
-
-        try {
-            fs.mkdirSync(dir);
-        }
-        catch (e) {
-        }
-
-        const manager = NeDBStore(path.join(dir, 'mqtt'));
-
-        manager.incoming.db.persistence.setAutocompactionInterval(60 * 1000);
-        manager.outgoing.db.persistence.setAutocompactionInterval(60 * 1000);
+        // Log current configurations
+        console.log('config.broker: ' + config.broker);
+        console.log('config.id: ' + config.id);
+        console.log('config.topic: ' + config.topic);
 
         const options = {
             keepalive: 10,
             clientId: config.id,
             clean: false,
             connectTimeout: 5 * 1000,
-            incomingStore: manager.incoming,
-            outgoingStore: manager.outgoing,
             queueQoSZero: false,
         };
 
         if (config.username && config.password) {
             options.username = config.username;
             options.password = config.password;
+        }
+
+        // Prepare directory for MQTT Store
+        const mqttStoreDir = path.join(RED.settings.userDir, 'glartek-collector/mqtt');
+
+        try {
+            if (!fs.existsSync(mqttStoreDir)) {
+                console.log("glartek-collector: Trying to create directory on ", mqttStoreDir);
+
+                // Create MQTT Store directory
+                fs.mkdirSync(mqttStoreDir, { recursive: true });
+            }
+
+            // Enable MQTT Store
+            const manager = NeDBStore(path.join(mqttStoreDir, 'mqtt'));
+            
+            manager.incoming.db.persistence.setAutocompactionInterval(60 * 1000);
+            manager.outgoing.db.persistence.setAutocompactionInterval(60 * 1000);
+
+            options.incomingStore = manager.incoming;
+            options.outgoingStore = manager.outgoing;
+        }
+        catch (e) {
+            node.error(e);
         }
 
         const client = mqtt.connect(config.broker, options);
