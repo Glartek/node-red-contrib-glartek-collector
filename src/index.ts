1e9f435e8901524501fd4fe6e33163213c89794e
Marco Santos <marco.santos@glartek.com>
2021-09-30 08:50:41 +0100

    chore: add latest changes

--- src/index.ts
+++ src/index.ts
@@ -1,160 +1,192 @@
-'use strict';
+"use strict";
 
-import mqtt from 'mqtt';
-import { NodeAPI, NodeAPISettingsWithData } from 'node-red';
-import { CollectorNode, CollectorConfig, MQTTNode } from './lib/types';
-import { CollectorState, CollectorStateType } from './lib/data';
+import mqtt from "mqtt";
+import { NodeAPI, NodeAPISettingsWithData } from "node-red";
+import { CollectorNode, CollectorConfig } from "./lib/types";
+import { CollectorState, CollectorStateType } from "./lib/data";
 
 export = (RED: NodeAPI<NodeAPISettingsWithData>): void =>
-    RED.nodes.registerType('glartek-collector', function (this: CollectorNode, config: CollectorConfig) {
-        const node: CollectorNode = this;
+  RED.nodes.registerType(
+    "glartek-collector",
+    function (this: CollectorNode, config: CollectorConfig) {
+      const node: CollectorNode = this;
 
-        RED.nodes.createNode(node, config);
+      RED.nodes.createNode(node, config);
 
-        // Clear node status
-        node.status({});
+      // Clear node status
+      node.status({});
 
-        // Log current configurations
-        console.log('glartek-collector: config.broker (' + config.broker + ')');
-        console.log('glartek-collector: config.clientid (' + config.clientid + ')');
-        console.log('glartek-collector: config.topic (' + config.topic + ')');
+      // Log current configurations
+      console.log("glartek-collector: config.broker (" + config.broker + ")");
+      console.log(
+        "glartek-collector: config.clientid (" + config.clientid + ")"
+      );
+      console.log("glartek-collector: config.topic (" + config.topic + ")");
 
-        const options = {
-            keepalive: 10,
-            clientId: config.clientid,
-            clean: true,
-            connectTimeout: 5 * 1000,
-            queueQoSZero: false,
-            username: '',
-            password: '',
-            rejectUnauthorized: false,
-            ca: '',
-            key: '',
-            cert: ''
-        };
+      const options: mqtt.IClientOptions = {
+        keepalive: 10,
+        clientId: config.clientid,
+        port: config.port,
+        clean: true,
+        connectTimeout: 5 * 1000,
+        queueQoSZero: false,
+        username: "",
+        password: "",
+        rejectUnauthorized: false,
+        ca: "",
+        key: "",
+        cert: "",
+      };
 
-        if (config.username && config.password) {
-            options.username = config.username;
-            options.password = config.password;
+      if (config.username && config.password) {
+        options.username = config.username;
+        options.password = config.password;
+      }
+
+      if (config.tls) {
+        options.rejectUnauthorized = true;
+        options.ca = config.tlsca;
+        options.key = config.tlspriv;
+        options.cert = config.tlscert;
+
+        const tlsAlpn = config.tlsalpn || "mqtt";
+        options.ALPNProtocols = tlsAlpn.split(",");
+      }
+
+      this.users = [];
+
+      this.connect = function () {
+        if (!node.connected && !node.connecting) {
+          node.connecting = true;
+
+          try {
+            const client = mqtt.connect(config.broker, options);
+            node.client = client;
+
+            // On mqtt connect
+            node.client.on("connect", function () {
+              const {
+                client: { options: mqttClientOptions },
+              } = node;
+
+              node.connecting = false;
+              node.connected = true;
+              node.log(
+                `collectorclient.mqtt.state.connected (${mqttClientOptions.clientId}@${mqttClientOptions.host}:${mqttClientOptions.port})`
+              );
+              for (var id in node.users) {
+                if (node.users.hasOwnProperty(id)) {
+                  node.users[id].status(
+                    CollectorState[CollectorStateType.CONNECTED]
+                  );
+                }
+              }
+            });
+
+            // On mqtt reconnect
+            node.client.on("reconnect", function () {
+              node.connected = false;
+              node.connecting = true;
+
+              for (var id in node.users) {
+                if (node.users.hasOwnProperty(id)) {
+                  node.users[id].status(
+                    CollectorState[CollectorStateType.RECONNECT]
+                  );
+                }
+              }
+            });
+
+            node.client.on("close", function () {
+              const {
+                client: { options: mqttClientOptions },
+              } = node;
+
+              // Is connection up?
+              if (node.connected) {
+                node.connected = false;
+                node.log(
+                  `collectorclient.mqtt.state.disconnected (${mqttClientOptions.clientId}@${mqttClientOptions.host}:${mqttClientOptions.port})`
+                );
+                for (var id in node.users) {
+                  if (node.users.hasOwnProperty(id)) {
+                    node.users[id].status(
+                      CollectorState[CollectorStateType.DISCONNECTED]
+                    );
+                  }
+                }
+              }
+
+              // Is connecting?
+              if (node.connecting) {
+                node.log(
+                  `collectorclient.mqtt.state.connect-failed (${mqttClientOptions.clientId}@${mqttClientOptions.host}:${mqttClientOptions.port})`
+                );
+              }
+            });
+
+            // On mqtt close
+            node.on("close", function (done: mqtt.CloseCallback) {
+              // Deregister
+              node.deregister(node, done);
+            });
+
+            // On mqtt error
+            node.client.on("error", function (error) {
+              node.error(error);
+            });
+
+            // On mqtt disconnect
+            node.client.on("disconnect", function () {
+              client.end();
+            });
+
+            node.on("input", function (msg) {
+              if (!client.connected) {
+                return;
+              }
+
+              if (msg.payload && typeof msg.payload === "object") {
+                client.publish(config.topic, JSON.stringify([msg.payload]), {
+                  qos: 1,
+                });
+              }
+            });
+          } catch (err) {
+            console.log(err);
+          }
+        }
+      };
+
+      // Register node
+      this.register = function (node: CollectorNode) {
+        node.users[node.id] = node;
+        if (Object.keys(node.users).length === 1) {
+          node.connect();
+        }
+      };
+
+      // DeRegister node
+      this.deregister = function (
+        node: CollectorNode,
+        done: mqtt.CloseCallback
+      ) {
+        delete node.users[node.id];
+        if (node.closing) {
+          return done();
+        }
+        if (Object.keys(node.users).length === 0) {
+          if (node.client && node.client.connected) {
+            return node.client.end(true);
+          } else {
+            node.client.end();
+            return done();
+          }
         }
 
-        if (config.tls) {
-            options.rejectUnauthorized = true;
-            options.ca = config.tlsca;
-            options.key = config.tlspriv;
-            options.cert = config.tlscert;
-            //options.protocol = 'mqtts';
-        }
+        done();
+      };
 
-        this.users = [];
-
-        this.connect = function () {
-            if (!node.connected && !node.connecting) {
-                node.connecting = true;
-
-                try {
-                    const client = mqtt.connect(config.broker, options)
-                    node.client = client
-                    
-                    // On mqtt connect
-                    node.client.on('connect', function () {
-                        node.connecting = false;
-                        node.connected = true;
-                        node.log(`collectorclient.mqtt.state.connected (${node.clientid}@${node.brokerurl})`);
-                        for (var id in node.users) {
-                            if (node.users.hasOwnProperty(id)) {
-                                node.users[id].status(CollectorState[CollectorStateType.CONNECTED]);
-                            }
-                        }
-                    });
-
-                    // On mqtt reconnect
-                    node.client.on('reconnect', function() {
-                        node.connected = false;
-                        node.connecting = true;
-
-                        for (var id in node.users) {
-                            if (node.users.hasOwnProperty(id)) {
-                                node.users[id].status(CollectorState[CollectorStateType.RECONNECT]);
-                            }
-                        }
-                    });
-
-                    node.client.on('close', function() {
-                         // Is connection up?
-                         if (node.connected) {
-                            node.connected = false;
-                            node.log(`collectorclient.mqtt.state.disconnected (${node.clientid}@${node.brokerurl})`);
-                            for (var id in node.users) {
-                                if (node.users.hasOwnProperty(id)) {
-                                    node.users[id].status(CollectorState[CollectorStateType.DISCONNECTED]);
-                                }
-                            }
-                        }
-                        
-                        // Is connecting?   
-                        if (node.connecting) {
-                            node.log(`collectorclient.mqtt.state.connect-failed (${node.clientid}@${node.brokerurl})`);
-                        }
-                    });
-
-                    // On mqtt close
-                    node.on('close', function (done: mqtt.CloseCallback) {
-                        // Deregister
-                        node.deregister(node, done);
-                    });
-
-                    // On mqtt error
-                    node.client.on('error', function(error) {
-                        node.error(error);
-                    });
-
-                    // On mqtt disconnect
-                    node.client.on('disconnect', function () {
-                        client.end()
-                    });
-
-                    node.on('input', function(msg) {
-                        if (!client.connected) {
-                            return
-                        }
-
-                        if (msg.payload && typeof msg.payload === 'object') {
-                            client.publish(config.topic, JSON.stringify([msg.payload]), { qos: 1 });
-                        }
-                    });
-
-                } catch(err) {
-                    console.log(err);
-                }
-            }
-        };
-
-        // Register node
-        this.register = function(node: CollectorNode) {
-            node.users[node.id] = node;
-            if (Object.keys(node.users).length === 1) {
-                node.connect();
-            }
-        };
-
-        // DeRegister node
-        this.deregister = function(node: CollectorNode, done: mqtt.CloseCallback) {
-            delete node.users[node.id];
-            if (node.closing) {
-                return done();
-            }
-            if (Object.keys(node.users).length === 0) {
-                if (node.client && node.client.connected) {
-                    return node.client.end(true);
-                } else {
-                    node.client.end();
-                    return done();
-                }
-            }
-
-            done();
-        };
-
-        node.register(this);
-    });
+      node.register(this);
+    }
+  );
--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,32 +1,34 @@
-import { MqttClient, CloseCallback } from 'mqtt';
-import { Node, NodeDef, NodeMessageInFlow } from 'node-red';
+import { MqttClient, CloseCallback } from "mqtt";
+import { Node, NodeDef, NodeMessageInFlow } from "node-red";
 
 export type CollectorNode = {
-    id: number 
-    connecting: boolean
-    connected: boolean
-    closing: boolean
-    clientid: string
-    brokerurl: string
-    client: MqttClient
-    users: Node[]
-    connect: Function
-    deregister: (node: CollectorNode, done: CloseCallback) => void | MqttClient
-    register: (node: CollectorNode) => void
+  id: number;
+  connecting: boolean;
+  connected: boolean;
+  closing: boolean;
+  clientid: string;
+  brokerurl: string;
+  client: MqttClient;
+  users: Node[];
+  connect: Function;
+  deregister: (node: CollectorNode, done: CloseCallback) => void | MqttClient;
+  register: (node: CollectorNode) => void;
 } & Node;
 
 export type CollectorConfig = {
-    clientid: string;
-    broker: string;
-    topic: string;
-    username: string;
-    password: string;
-    tls: boolean;
-    tlsca: string;
-    tlspriv: string;
-    tlscert: string;
+  clientid: string;
+  broker: string;
+  port: number;
+  topic: string;
+  username: string;
+  password: string;
+  tls: boolean;
+  tlsca: string;
+  tlspriv: string;
+  tlscert: string;
+  tlsalpn: string;
 } & NodeDef;
 
 export type MQTTNode = {
-    id: number
+  id: number;
 } & Node;
